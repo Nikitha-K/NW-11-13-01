0fad2383783381562af962c401bcb3e8a9b030c9
Nikitha K <32413846+Nikitha-K@users.noreply.github.com>
2020-01-13 03:58:54 -0800

    Add files via upload

---
+++ HelloSpringBoot/src/main/java/boot/HelloSpringBoot/HelloSpringBootApplication.java
@@ -0,0 +1,15 @@
+package boot.HelloSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(HelloSpringBootApplication.class,args);
+		
+	}
+
+}
